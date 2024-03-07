77a496915f4cbe65c5e3e569f96458fcc13e39f5
SPBurns12 <sean_p_burns@progressive.com>
2024-03-07 12:40:30 -0500

    began using substitute package

--- src/week1/Banking Solution/Banking.UnitTests/GoldCustomerDeposits.cs
+++ src/week1/Banking Solution/Banking.UnitTests/GoldCustomerDeposits.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using Bank;
+using NSubstitute;
 
 namespace Banking.UnitTests.Accounts;
 public class GoldCustomerDeposits
@@ -9,16 +10,19 @@ public class GoldCustomerDeposits
     public void MakingADespositIncreasesBalance()
     {
         // Given
-        var account = new BankAccount(new StubbedBonusCalculator());
-        // Get Balance is a "Query" - we are asking it for something.
+        var amountToDeposit = 142.23M;
+        var expectedBonus = 13.23M;
+        var stubbedBonusCalculator = Substitute.For<ICalculateBonusesForDeposits>();
+        var account = new BankAccount(stubbedBonusCalculator);
         var openingBalance = account.GetBalance();
+        stubbedBonusCalculator.CalculateBonus(openingBalance, amountToDeposit).Returns(expectedBonus);
 
         // When
         // WTCYWYH 
         // Command - Telling it to do some work.
-        account.Deposit(142.23M);
+        account.Deposit(amountToDeposit);
 
         // Then
-        Assert.Equal(openingBalance + 142.23M + 13.23M, account.GetBalance());
+        Assert.Equal(openingBalance + amountToDeposit + expectedBonus, account.GetBalance());
     }
 }
