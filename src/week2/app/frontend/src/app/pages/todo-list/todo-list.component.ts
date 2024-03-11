8d45dfbd4f2708be4ac2420216cbf6c59ba19365
SPBurns12 <sean_p_burns@progressive.com>
2024-03-11 16:36:55 -0400

    End of day

--- src/week2/app/frontend/src/app/pages/todo-list/todo-list.component.ts
+++ src/week2/app/frontend/src/app/pages/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoEntryComponent } from './components/todo-entry/todo-entry.component';
 import { TodoItemListComponent } from './components/todo-item-list/todo-item-list.component';
+import { TodoListItem } from './models';
 
 @Component({
   selector: 'app-todo-list',
@@ -10,13 +11,27 @@ import { TodoItemListComponent } from './components/todo-item-list/todo-item-lis
       <h2 class="text-2xl font-bold">Your Todo List</h2>
     </section>
     <div>
-      <app-todo-entry />
+      <app-todo-entry (itemAdded)="addItem($event)" />
     </div>
     <div>
-      <app-todo-item-list />
+      <app-todo-item-list [list]="todoList" />
     </div>
   `,
   styles: ``,
   imports: [TodoEntryComponent, TodoItemListComponent],
 })
-export class TodoListComponent {}
+export class TodoListComponent {
+  addItem(item: { description: string }) {
+    const itemToAdd: TodoListItem = {
+      id: crypto.randomUUID(),
+      description: item.description,
+      completed: false,
+    };
+    this.todoList = [itemToAdd, ...this.todoList];
+  }
+
+  todoList: TodoListItem[] = [
+    { id: '3', description: 'Learn Signals', completed: false },
+    { id: '4', description: 'Learn Redux', completed: true },
+  ];
+}
