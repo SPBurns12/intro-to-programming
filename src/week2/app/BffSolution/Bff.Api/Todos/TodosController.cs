e3263a09460d639b7180e49115c2cf224c969478
SPBurns12 <sean_p_burns@progressive.com>
2024-03-13 16:37:29 -0400

    end of day wednesday

--- src/week2/app/BffSolution/Bff.Api/Todos/TodosController.cs
+++ src/week2/app/BffSolution/Bff.Api/Todos/TodosController.cs
@@ -1,24 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bff.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bff.Api.Todos;
 
-public class TodosController : ControllerBase
+public class TodosController(TodosDataContext _context) : ControllerBase
 {
+    [HttpGet("/todos")]
+    public async Task<IActionResult> GetAllTodosAsync()
+    {
+        var response = await _context.Todos.ToListAsync();
+        return Ok(response);
+    }
+
     [HttpPost("/todos")]
-    public IActionResult AddATodo([FromBody] CreateTodoRequest request)
+    public async Task<IActionResult> AddATodo([FromBody] CreateTodoRequest request)
     {
         // validate it - description is required, min length 3, maximum length 150
         //               dueDate >= Today's Date
         // If it's not valid, return a 400. 
         // if it is valid -
         // Write something to the database (we are stateless here)
+        var todoToAdd = new TodoEntity
+        {
+            Description = request.Description,
+            CreatedDate = DateTimeOffset.Now,
+            DueDate = request.DueDate,
+            Priority = request.Priority
+        };
+        _context.Todos.Add(todoToAdd);
+        await _context.SaveChangesAsync();
         // send them back 
         var response = new CreateTodoResponse
         {
-            Id = Guid.NewGuid(),
-            Description = request.Description,
-            DueDate = request.DueDate,
-            Priority = request.Priority
+            Id = todoToAdd.Id,
+            Description = todoToAdd.Description,
+            DueDate = todoToAdd.DueDate,
+            Priority = todoToAdd.Priority
         };
         return Ok(response);
     }
